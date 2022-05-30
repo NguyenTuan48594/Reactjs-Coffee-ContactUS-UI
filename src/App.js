5f000c164f6b79b156189343a5374269b14756ab
unknown <tuan48594@donga.edu.vn>
2022-05-30 15:46:33 +0700

    first commit ui

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Email } from "./components/ContactUs";
+
+function App() {
+  return (
+    <div className="App">
+      <Email />
+    </div>
+  );
+}
+
+export default App;
